ded3759651916405047a41cce88c2023aae0fe4e
Julien Montagnon <jumontagnon@gmail.com>
2020-06-04 11:33:29 +0200

    Updates

---
+++ precache-manifest.038d45e8a3fe02595716f9dd632f34c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cc6a15d0b6e62a40b82d2c47833308d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "86aaa70e88e036e184f4",
+    "url": "./static/css/main.32cb2023.chunk.css"
+  },
+  {
+    "revision": "4cbebe21bff79d295e77",
+    "url": "./static/js/2.0ff7ac41.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0ff7ac41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86aaa70e88e036e184f4",
+    "url": "./static/js/main.7263bea5.chunk.js"
+  },
+  {
+    "revision": "d44b78b73efcd7116c6e",
+    "url": "./static/js/runtime-main.e9fad991.js"
+  }
+]);

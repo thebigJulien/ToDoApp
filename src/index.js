ee9a5fab66c1eabf638d1fa8ce84a9b92df53258
Julien Montagnon <jumontagnon@gmail.com>
2020-07-09 18:13:47 +0200

    till naive component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,25 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
-// import * as serviceWorker from './serviceWorker';
+
+
+// create a react component
+
+const App = () => {
+  return (
+  <div>
+    <label className="label" htmlFor="name">Enter name:</label>
+    <input id="name" type="text" />
+    <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+  </div>
+  );
+};
+
+
+// take the react component and show it on the screen
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <App />,
+  document.querySelector('#root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
